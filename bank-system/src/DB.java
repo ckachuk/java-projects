9002df7200eeb14aca94d70d3d4e811709803f0e
carloskachuk <carloskachuk@gmail.com>
2024-09-09 17:28:53 -0300

    feat: db connection has been implemented

--- bank-system/src/DB.java
+++ bank-system/src/DB.java
@@ -6,7 +6,6 @@ public class DB {
     private static String USER = "postgres";
     private static String PASSWORD = "elensar15";
 
-
     public static Connection connect() throws SQLException {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
--- bank-system/src/Main.java
+++ bank-system/src/Main.java
@@ -1,7 +1,15 @@
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("hello world!");
+        System.out.printf("hello world!\n");
+
+        try(var connection = DB.connect()){
+            System.out.println("Connected to the PostgreSQL database.");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
